4b98388d28b165246fc394e93258e9b1068a6116
Kent Butler <KentDavidButler@gmail.com>
2019-02-23 21:42:34 -0500

    Testing retreiving data from API

---
+++ Movie API Project/Models/MovieDAL.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Movie_API_Project.Models
+{
+    public class MovieDAL
+    {
+        
+        public static string GetData(string Url)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(Url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string data = rd.ReadToEnd();
+            JObject movieJson = JObject.Parse(data);
+            string title = movieJson["Search"][0]["Title"].ToString();
+            return title;
+        }
+
+        public static string GetSearchResult(string search)
+        {
+            //http://www.omdbapi.com/?i=tt3896198&apikey=c2c5c13d
+            //string url = "http://www.omdbapi.com/?s=" + txtMovieName.Text.Trim() + "&apikey=XXXX";
+
+            string output = GetData("http://www.omdbapi.com/?s=" + search.Trim() + "&apikey=c2c5c13d");
+            //RedditPost rp = new RedditPost(output, i);
+
+            return output;
+        }
+    }
+}
