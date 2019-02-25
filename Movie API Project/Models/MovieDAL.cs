0d22abc7ccd864795377c865e8b8d1d6100bb1a1
Kent Butler <KentDavidButler@gmail.com>
2019-02-25 09:11:07 -0500

    trying to figure out API pulls

--- Movie API Project/Models/MovieDAL.cs
+++ Movie API Project/Models/MovieDAL.cs
@@ -11,7 +11,7 @@ namespace Movie_API_Project.Models
     public class MovieDAL
     {
         
-        public static string GetData(string Url)
+        public static List<string> GetData(string Url)
         {
             HttpWebRequest request = WebRequest.CreateHttp(Url);
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
@@ -19,16 +19,24 @@ namespace Movie_API_Project.Models
             StreamReader rd = new StreamReader(response.GetResponseStream());
             string data = rd.ReadToEnd();
             JObject movieJson = JObject.Parse(data);
-            string title = movieJson["Search"][0]["Title"].ToString();
-            return title;
+            // string title = movieJson["Search"][0]["Title"].ToString();
+            // return title;
+
+            List<JToken> searchResults = movieJson["Search"].ToList();
+            List<string> titleList = new List<string>();
+            foreach (var titles in searchResults)
+            {
+                titleList.Add(titles["Title"].ToString());
+            }
+            return titleList;
         }
 
-        public static string GetSearchResult(string search)
+        public static List<string> GetSearchResult(string search)
         {
             //http://www.omdbapi.com/?i=tt3896198&apikey=c2c5c13d
             //string url = "http://www.omdbapi.com/?s=" + txtMovieName.Text.Trim() + "&apikey=XXXX";
 
-            string output = GetData("http://www.omdbapi.com/?s=" + search.Trim() + "&apikey=c2c5c13d");
+            List<string> output = GetData("http://www.omdbapi.com/?s=" + search.Trim() + "&apikey=c2c5c13d");
             //RedditPost rp = new RedditPost(output, i);
 
             return output;
